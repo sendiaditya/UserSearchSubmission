41beae6bd665f06f49b3f4400c0901b1d8c6a2d0
Sendi <sendiaditya@gmail.com>
2024-03-17 17:28:03 +0700

    search with livedata

--- app/src/main/java/com/example/usersearchsubmission/ui/MainViewModel.kt
+++ app/src/main/java/com/example/usersearchsubmission/ui/MainViewModel.kt
@@ -1,24 +1,62 @@
 package com.example.usersearchsubmission.ui
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.usersearchsubmission.data.response.GithubResponse
 import com.example.usersearchsubmission.data.response.UserItem
+import com.example.usersearchsubmission.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
-    private val _photo = MutableLiveData<String>()
-    val photo: LiveData<String> = _photo
-    private val _listUser  = MutableLiveData<List<UserItem>>()
-    val listReview : LiveData<List<UserItem>> = _listUser
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
+
     companion object{
         private const val TAG = "MainActivity"
         private var USERNAME = "ahmad"
     }
 
+    private val _listUser  = MutableLiveData<List<UserItem>>()
+    val listUser : LiveData<List<UserItem>> = _listUser
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+    val _searchQuery = MutableLiveData<String>()
+    val searchQuery: LiveData<String> = _searchQuery
+
     init {
-        findRestaurant()
+        findUser()
     }
 
+    private fun findUser() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getUsers(USERNAME)
+        client.enqueue(object : Callback<GithubResponse> {
+            override fun onResponse(
+                call: Call<GithubResponse>,
+                response: Response<GithubResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _listUser.value = response.body()?.items
+                    }
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+    init {
+        _searchQuery.observeForever { query ->
+            USERNAME = query
+            findUser()
+        }
+    }
 }
